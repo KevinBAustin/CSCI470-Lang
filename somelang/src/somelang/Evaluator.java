a66c81c647c7c684ea93fd6d696a86ac5a5181ef
TuckerSelf <122940720+TuckerSelf@users.noreply.github.com>
2025-04-16 15:55:38 -0500

    Update Evaluator.java

--- somelang/src/somelang/Evaluator.java
+++ somelang/src/somelang/Evaluator.java
@@ -12,4 +12,76 @@ import somelang.Env.*;
 
 public class Evaluator implements AST.Visitor<Value> {
 
+  Env initEnv = initialEnv(); //New for definelang
+	
+	Value valueOf(Program p) {
+			return (Value) p.accept(this, initEnv);
+	}
+	
+	@Override
+	public Value visit(AddExp e, Env env) {
+		List<Exp> operands = e.all();
+		double result = 0;
+		for(Exp exp: operands) {
+			NumVal intermediate = (NumVal) exp.accept(this, env); // Dynamic type-checking
+			result += intermediate.v(); //Semantics of AddExp in terms of the target language.
+		}
+		return new NumVal(result);
+	}
+	
+	@Override
+	public Value visit(UnitExp e, Env env) {
+		return new UnitVal();
+	}
+
+	@Override
+	public Value visit(NumExp e, Env env) {
+		return new NumVal(e.v());
+	}
+
+	@Override
+	public Value visit(StrExp e, Env env) {
+		return new StringVal(e.v());
+	}
+
+	@Override
+	public Value visit(BoolExp e, Env env) {
+		return new BoolVal(e.v());
+	}
+
+	@Override
+	public Value visit(DivExp e, Env env) {
+		List<Exp> operands = e.all();
+		NumVal lVal = (NumVal) operands.get(0).accept(this, env);
+		double result = lVal.v(); 
+		for(int i=1; i<operands.size(); i++) {
+			NumVal rVal = (NumVal) operands.get(i).accept(this, env);
+			result = result / rVal.v();
+		}
+		return new NumVal(result);
+	}
+
+	@Override
+	public Value visit(MultExp e, Env env) {
+		List<Exp> operands = e.all();
+		double result = 1;
+		for(Exp exp: operands) {
+			NumVal intermediate = (NumVal) exp.accept(this, env); // Dynamic type-checking
+			result *= intermediate.v(); //Semantics of MultExp.
+		}
+		return new NumVal(result);
+	}
+
+  @Override
+	public Value visit(SubExp e, Env env) {
+		List<Exp> operands = e.all();
+		NumVal lVal = (NumVal) operands.get(0).accept(this, env);
+		double result = lVal.v();
+		for(int i=1; i<operands.size(); i++) {
+			NumVal rVal = (NumVal) operands.get(i).accept(this, env);
+			result = result - rVal.v();
+		}
+		return new NumVal(result);
+	}
+
 }
