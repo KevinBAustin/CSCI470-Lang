e8d39bcacc4bfcb9c992c61b7049dd3c8e713ee2
TuckerSelf <122940720+TuckerSelf@users.noreply.github.com>
2025-04-16 14:55:31 -0500

    Update AST.java

--- somelang/src/somelang/AST.java
+++ somelang/src/somelang/AST.java
@@ -1,1 +1,10 @@
+public interface AST {
+	public static abstract class ASTNode implements AST {
+		public abstract <T> T accept(Visitor<T> visitor, Env env);
+	}
 
+
+  public interface Visitor <T> {
+    public T visit(AST., Env env);
+  }
+}
