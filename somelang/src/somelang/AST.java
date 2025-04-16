b701fe5d7e441492d8a9d99019458b388567e251
TuckerSelf <122940720+TuckerSelf@users.noreply.github.com>
2025-04-16 14:59:17 -0500

    Update AST.java

--- somelang/src/somelang/AST.java
+++ somelang/src/somelang/AST.java
@@ -2,7 +2,121 @@ public interface AST {
 	public static abstract class ASTNode implements AST {
 		public abstract <T> T accept(Visitor<T> visitor, Env env);
 	}
+	public static class Program extends ASTNode {
+		List<DefineDecl> _decls;
+		Exp _e;
 
+		public Program(List<DefineDecl>decls, Exp e) {
+			_decls = decls;
+			_e = e;
+		}
+
+		public Exp e() {
+			return _e;
+		}
+		
+		public List<DefineDecl> decls() {
+			return _decls;
+		}
+		
+		public <T> T accept(Visitor<T> visitor, Env env) {
+			return visitor.visit(this, env);
+		}
+	}
+	public static abstract class Exp extends ASTNode {
+
+	}
+
+	/*Insert other functionalities here*/
+
+	public static class AddExp extends CompoundArithExp {
+		public AddExp(Exp fst) {
+			super(fst);
+		}
+
+		public AddExp(List<Exp> args) {
+			super(args);
+		}
+
+		public AddExp(Exp fst, List<Exp> rest) {
+			super(fst, rest);
+		}
+
+		public AddExp(Exp left, Exp right) {
+			super(left, right);
+		}
+		
+		public <T> T accept(Visitor<T> visitor, Env env) {
+			return visitor.visit(this, env);
+		}
+	}
+
+	public static class SubExp extends CompoundArithExp {
+
+		public SubExp(Exp fst) {
+			super(fst);
+		}
+
+		public SubExp(List<Exp> args) {
+			super(args);
+		}
+
+		public SubExp(Exp fst, List<Exp> rest) {
+			super(fst, rest);
+		}
+
+		public SubExp(Exp left, Exp right) {
+			super(left, right);
+		}
+		
+		public <T> T accept(Visitor<T> visitor, Env env) {
+			return visitor.visit(this, env);
+		}
+	}
+
+	public static class DivExp extends CompoundArithExp {
+		public DivExp(Exp fst) {
+			super(fst);
+		}
+
+		public DivExp(List<Exp> args) {
+			super(args);
+		}
+
+		public DivExp(Exp fst, List<Exp> rest) {
+			super(fst, rest);
+		}
+
+		public DivExp(Exp left, Exp right) {
+			super(left, right);
+		}
+		
+		public <T> T accept(Visitor<T> visitor, Env env) {
+			return visitor.visit(this, env);
+		}
+	}
+
+	public static class MultExp extends CompoundArithExp {
+		public MultExp(Exp fst) {
+			super(fst);
+		}
+
+		public MultExp(List<Exp> args) {
+			super(args);
+		}
+
+		public MultExp(Exp fst, List<Exp> rest) {
+			super(fst, rest);
+		}
+
+		public MultExp(Exp left, Exp right) {
+			super(left, right);
+		}
+		
+		public <T> T accept(Visitor<T> visitor, Env env) {
+			return visitor.visit(this, env);
+		}
+	}
 
   public interface Visitor <T> {
     public T visit(AST., Env env);
