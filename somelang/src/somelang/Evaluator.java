f266485545902fbb017bb956b95c35f3b6cf1734
KevinBAustin <122644452+KevinBAustin@users.noreply.github.com>
2025-04-15 20:19:14 -0500

    Update Evaluator.java

--- somelang/src/somelang/Evaluator.java
+++ somelang/src/somelang/Evaluator.java
@@ -1,1 +1,15 @@
+package annoylang;
 
+import java.util.List;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
+
+import static annoylang.AST.*;
+import static annoylang.Value.*;
+import annoylang.Env.*;
+
+public class Evaluator implements AST.Visitor<Value> {
+
+}
