519f5bd1e7e1deabb5178e14abdf233d5f290323
TuckerSelf <122940720+TuckerSelf@users.noreply.github.com>
2025-04-16 18:35:24 -0500

    Update Evaluator.java

--- somelang/src/somelang/Evaluator.java
+++ somelang/src/somelang/Evaluator.java
@@ -45,11 +45,6 @@ public class Evaluator implements AST.Visitor<Value> {
 	}
 
 	@Override
-	public Value visit(BoolExp e, Env env) {
-		return new BoolVal(e.v());
-	}
-
-	@Override
 	public Value visit(DivExp e, Env env) {
 		List<Exp> operands = e.all();
 		NumVal lVal = (NumVal) operands.get(0).accept(this, env);
@@ -57,6 +52,18 @@ public class Evaluator implements AST.Visitor<Value> {
 		for(int i=1; i<operands.size(); i++) {
 			NumVal rVal = (NumVal) operands.get(i).accept(this, env);
 			result = result / rVal.v();
+		}
+		return new NumVal(result);
+	}
+
+	@Override
+	public Value visit(RemExp e, Env env) {
+		List<Exp> operands = e.all();
+		NumVal lVal = (NumVal) operands.get(0).accept(this, env);
+		double result = lVal.v(); 
+		for(int i=1; i<operands.size(); i++) {
+			NumVal rVal = (NumVal) operands.get(i).accept(this, env);
+			result = result % rVal.v();
 		}
 		return new NumVal(result);
 	}
@@ -82,6 +89,51 @@ public class Evaluator implements AST.Visitor<Value> {
 			result = result - rVal.v();
 		}
 		return new NumVal(result);
+	}
+
+	@Override
+	public Value visit(Program p) {
+		return (Value) p.e().accept(this, env);
+	}
+
+	@Override
+	public Value visit(IsExp e, Env env) { // New for definelang.
+		String name = e.name();
+		Exp value_exp = e.value_exp();
+		Value value = (Value) value_exp.accept(this, env);
+		((GlobalEnv) initEnv).extend(name, value);
+		return new Value.UnitVal();		
+	}
+
+	@Override
+	public Value visit(NullExp e, Env env) {
+		Value val = (Value) e.arg().accept(this, env);
+		return new BoolVal(val instanceof Value.Null);
+	}
+
+	/*Comp Evaluations*/
+
+	/*The following is last*/
+	private Env initialEnv() {
+		GlobalEnv initEnv = new GlobalEnv();
+		
+		/* Procedure: (read <filename>). Following is same as (define read (lambda (file) (read file))) */
+		List<String> formals = new ArrayList<>();
+		formals.add("file");
+		Exp body = new AST.ReadExp(new VarExp("file"));
+		Value.FunVal readFun = new Value.FunVal(initEnv, formals, body);
+		initEnv.extend("read", readFun);
+
+		/* Procedure: (require <filename>). Following is same as (define require (lambda (file) (eval (read file)))) */
+		formals = new ArrayList<>();
+		formals.add("file");
+		body = new EvalExp(new AST.ReadExp(new VarExp("file")));
+		Value.FunVal requireFun = new Value.FunVal(initEnv, formals, body);
+		initEnv.extend("require", requireFun);
+		
+		/* Add new built-in procedures here */ 
+		
+		return initEnv;
 	}
 
 }
